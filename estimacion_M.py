6de4739d20ba5f10b57a71424dff63d645d9c8eb
Carlos Valverde <z72vamac@uco.es>
2023-01-26 01:38:07 +0100

    Inicializando repositorio

---
+++ estimacion_M.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 00:16:23 2019
+
+@author: carlo
+"""
+from gurobipy import *
+import numpy as np
+from neighborhood import *
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+import auxiliar_functions as af
+
+
+# def estima_BigM(data):
+#
+#     m = len(data)
+#     BigM = 0
+#
+#     for i in range(m):
+#         for j in range(m):
+#             if i != j:
+#                 comp1 = data[i]
+#                 comp2 = data[j]
+#
+#                 if type(comp1) is Poligono or Poligonal:
+#                     if type(comp2) is Poligono:
+#                         maximo = max([np.linalg.norm(v-w) for v in comp1.V
+#                                                           for w in comp2.V])
+#
+#                     if type(comp2) is Elipse:
+#                         maximo = comp2.radio + max([np.linalg.norm(v-comp2.centro) for v in comp1.V])
+#
+#                 if type(comp1) is Poligonal:
+#                     if type(comp2) is Poligono or Poligonal:
+#                         maximo = max([np.linalg.norm(w-v) for w in comp2.V
+#                                                           for v in comp1.V])
+#
+#                     if type(comp2) is Elipse:
+#                         caso1 = comp2.radio + np.linalg.norm(comp1.P-comp2.centro)
+#                         caso2 = comp2.radio + np.linalg.norm(comp1.Q-comp2.centro)
+#                         maximo = max(caso1, caso2)
+#
+#                 if type(comp1) is Elipse:
+#                     if type(comp2) is Poligono or Poligonal:
+#                         maximo = comp1.radio + max([np.linalg.norm(comp1.centro-w) for w in comp2.V])
+#
+#                     if type(comp2) is Elipse:
+#                         maximo = comp1.radio + np.linalg.norm(comp1.centro-comp2.centro) + comp2.radio
+#
+#                 if maximo >= BigM:
+#                     BigM = maximo
+#
+#     return BigM
+
+
+def estima_BigM_local(comp1, comp2):
+        maximo = 0
+        if type(comp1) is Poligono or type(comp1) is Poligonal:
+            if type(comp2) is Poligono or type(comp2) is Poligonal:
+                maximo = max([np.linalg.norm(v-w) for v in comp1.V
+                                                  for w in comp2.V])
+
+            if type(comp2) is Elipse:
+                maximo = comp2.radio + max([np.linalg.norm(v-comp2.centro) for v in comp1.V])
+
+        if type(comp1) is Elipse:
+            if type(comp2) is Poligono or type(comp2) is Poligonal:
+                maximo = comp1.radio + max([np.linalg.norm(comp1.centro-w) for w in comp2.V])
+
+            if type(comp2) is Elipse:
+                maximo = comp1.radio + np.linalg.norm(comp1.centro-comp2.centro) + comp2.radio
+
+        return maximo
+
+def estima_SmallM_local(comp1, comp2):
+        if type(comp1) is Poligono or type(comp1) is Poligonal:
+            if type(comp2) is Poligono or type(comp2) is Poligonal:
+                minimo = min([np.linalg.norm(v-w) for v in comp1.V
+                                                  for w in comp2.V])
+
+            if type(comp2) is Elipse:
+                minimo = - comp2.radio + min([np.linalg.norm(v-comp2.centro) for v in comp1.V])
+
+        if type(comp1) is Elipse:
+            if type(comp2) is Poligono or type(comp2) is Poligonal:
+                minimo = -comp1.radio + min([np.linalg.norm(comp1.centro-w) for w in comp2.V])
+
+            if type(comp2) is Elipse:
+                minimo = -comp1.radio + np.linalg.norm(comp1.centro-comp2.centro) - comp2.radio
+
+        return minimo
+
+def estima_max_inside(comp):
+        maximo = 0
+        if type(comp) is Poligono:
+            maximo = max([np.linalg.norm(v-w) for v in comp.V for w in comp.V])
+
+        if type(comp) is Elipse:
+            maximo = 2*comp.radio
+
+        if type(comp) is Poligonal:
+            maximo = comp.alpha * comp.longitud
+
+        return maximo
+
+def estima_M_alpha1(entorno, punto1, punto2):
+    if type(entorno) is Circle:
+
+        theta = np.linspace(0, 2*np.pi, 100)
+
+        centro = entorno.center
+        radio = entorno.radii
+
+        x = centro[0] + radio*np.cos(theta)
+        y = centro[1] + radio*np.sin(theta)
+
+        # print([x[0], y[0]])
+
+        determinantes = [af.determinant([x[i], y[i]], punto1, punto2) for i in range(100)]
+
+        m = -100*abs(min(determinantes))
+        M = 100*abs(max(determinantes))
+
+        return m, M
+
+def estima_M_alpha2(punto1, entorno, punto2):
+    if type(entorno) is Circle:
+
+        theta = np.linspace(0, 2*np.pi, 100)
+
+        centro = entorno.center
+        radio = entorno.radii
+
+        x = centro[0] + radio*np.cos(theta)
+        y = centro[1] + radio*np.sin(theta)
+
+        # print([x[0], y[0]])
+
+        determinantes = [af.determinant(punto1, [x[i], y[i]], punto2) for i in range(100)]
+
+        m = -100*abs(min(determinantes))
+        M = 100*abs(max(determinantes))
+
+        return m, M
+
+def estima_M_alpha3(punto1, punto2, entorno):
+    if type(entorno) is Circle:
+
+        theta = np.linspace(0, 2*np.pi, 100)
+
+        centro = entorno.center
+        radio = entorno.radii
+
+        x = centro[0] + radio*np.cos(theta)
+        y = centro[1] + radio*np.sin(theta)
+
+        # print([x[0], y[0]])
+
+        determinantes = [af.determinant(punto1, punto2, [x[i], y[i]]) for i in range(100)]
+
+        m = -100*abs(min(determinantes))
+        M = 100*abs(max(determinantes))
+
+        return m, M
+
+def estima_M_alpha4(punto1, entorno1, entorno2):
+    if type(entorno1) is Circle and type(entorno2) is Circle:
+
+        theta = np.linspace(0, 2*np.pi, 100)
+
+        centro1 = entorno1.center
+        radio1 = entorno1.radii
+
+        x1 = centro1[0] + radio1*np.cos(theta)
+        y1 = centro1[1] + radio1*np.sin(theta)
+
+        centro2 = entorno2.center
+        radio2 = entorno2.radii
+
+        x2 = centro2[0] + radio2*np.cos(theta)
+        y2 = centro2[1] + radio2*np.sin(theta)
+
+        # print([x[0], y[0]])
+
+        determinantes = [af.determinant(punto1, [x1[i], y1[i]], [x2[j], y2[j]]) for i in range(100) for j in range(100)]
+
+        m = -100*abs(min(determinantes))
+        M = 100*abs(max(determinantes))
+
+        return m, M
---
+++ main.py
@@ -0,0 +1,148 @@
+import gurobipy as gp
+from gurobipy import GRB
+import numpy as np
+from itertools import product, permutations, chain
+import random
+import matplotlib.pyplot as plt
+# from matplotlib.patches import Circle, Polygon
+from matplotlib.collections import PatchCollection
+from data import *
+import neighborhood as neigh
+import copy
+import estimacion_M as eM
+import auxiliar_functions as af
+import networkx as nx
+
+from spp_barriers import hspp_b
+
+# from HTSPS_without_prepro import HTSPS_without_prepro
+# 50-3
+
+# segments = np.genfromtxt('./instancias/segmentos50-3.csv', delimiter = ',')
+
+# barriers = []
+# for lista in segments:
+#     barriers.append([[lista[0], lista[1]], [lista[2], lista[3]]])
+
+# bolas = np.genfromtxt('./instancias/bolas50-3.csv', delimiter = ',')
+# sources = [neigh.Circle(center = [centro1, centro2], radii = radio, col = 'blue') for centro1, centro2, radio in bolas]
+
+# targets = [neigh.Circle(center = [centro1, centro2], radii = radio, col = 'red') for centro1, centro2, radio in bolas]
+#
+# k = 3
+#
+# resultados = h_kmedian_n(barriers, sources, targets, k, time_limit=3600, prepro=True, log=False, picture=True)
+
+# from h_kmedian_n_moresubindex import h_kmedian_n
+# from h_kmedian_n import h_kmedian_n
+#
+## EXAMPLE NON-VISIBLE
+
+barrier1 = [[0, 90], [30, 60]]
+barrier2 = [[10, 50], [40, 50]]
+barrier3 = [[0, 30], [10, 40]]
+barrier4 = [[10, 30], [30, 5]]
+barrier5 = [[40, 10], [70, 40]]
+barrier6 = [[60, 20], [100, 10]]
+barrier7 = [[30, 70], [70, 95]]
+barrier8 = [[70, 90], [60, 50]]
+barrier9 = [[70, 80], [90, 60]]
+barrier10 = [[74, 33], [98, 60]]
+
+barriers = [barrier1, barrier2, barrier3, barrier4, barrier5, barrier6, barrier7, barrier8, barrier9, barrier10]
+# barriers = [barrier3, barrier5, barrier6, barrier7, barrier8]
+
+N1s = neigh.Circle(center=[70, 55], radii=4, col = 'green')
+N2s = neigh.Circle(center=[50, 70], radii=8, col = 'green')
+N3s = neigh.Circle(center=[30, 35], radii=10, col = 'green')
+
+sources = [N1s, N2s, N3s]
+sources = [N1s]
+
+N1t = neigh.Circle(center=[10, 15], radii=6, col = 'blue')
+N2t = neigh.Circle(center=[65, 10], radii=7, col = 'blue')
+N3t = neigh.Circle(center=[10, 65], radii=5, col = 'blue')
+N4t = neigh.Circle(center=[90, 35], radii=6, col = 'blue')
+N5t = neigh.Circle(center=[90, 85], radii=6, col = 'blue')
+N6t = neigh.Circle(center=[30, 90], radii=10, col = 'blue')
+
+targets = [N1t, N2t, N3t, N4t, N5t, N6t]
+targets = [N1t]
+
+k = 3
+
+endurance = 1000
+
+wE = 1
+
+wL = 0
+
+# 279.88
+# resultados = hspp_b(barriers, sources=sources, targets=targets, k=k, wL=wL, lazy=False, A4=False, time_limit=1800, picture=True, init = False)
+
+# resultados = hspp_b(barriers, sources, targets, wL = 0, A4 = True, log=False, picture=True, time_limit=7200, init=False)
+
+# print(resultados)
+
+
+# fig, ax = plt.subplots()
+
+x_coords = list(np.linspace(0, 100, 1001))
+y_coords = list(np.linspace(0, 100, 1001))
+
+distancias = np.zeros((1000, 1000))
+
+# for b in barriers:
+#     ax.plot([b[0][0], b[1][0]], [b[0][1], b[1][1]], c='red')
+
+
+for i in range(642, 645):
+    for j in range(1000):
+        targets = [neigh.Circle(center=[x_coords[i], y_coords[j]], radii=0.01, col = 'blue')]
+        resultados = hspp_b(barriers, sources, targets, wL = 0, A4 = False, log=False, picture=False, time_limit=7200, init=False)
+
+        distancias[i, j] = resultados[1]
+
+        # if resultados[1] < radii:
+        #     ax.scatter(xcor, ycor, color = 'blue')
+        # else:
+        #     ax.scatter(xcor, ycor, color = 'red')
+
+    np.savetxt('distancias_masbarreras.csv', distancias, delimiter=',')
+# ax.scatter(sources[0].center[0], sources[0].center[1], c='green')
+
+# plt.axis([0, 100, 0, 100])
+
+# ax.set_aspect('equal')
+# plt.show()
+
+
+
+
+# radii = 30
+
+# for i in range(100):
+#     for j in range(100):
+#         if distancias[i, j] < radii:
+#             ax.scatter(x_coords[i], y_coords[j], color = 'blue')
+#         else:
+#             ax.scatter(x_coords[i], y_coords[j], color = 'red')
+
+# for b in barriers:
+#     ax.plot([b[0][0], b[1][0]], [b[0][1], b[1][1]], c='red')
+
+# plt.axis([0, 100, 0, 100])
+
+# ax.set_aspect('equal')
+# plt.show()
+
+# import seaborn as sns
+
+# distancias = np.genfromtxt('distancias.csv', delimiter = ',').T
+
+# fig, ax = plt.subplots()
+
+# sns.heatmap(distancias[::-1, :], cmap = 'Greys')
+
+# plt.show()
+
