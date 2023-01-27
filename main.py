6e1707a4455d50d68114e69e34f30aa11bd3e1cf
Carlos Valverde <z72vamac@uco.es>
2023-01-27 12:22:42 +0100

    Matriz de nuevas distancias realizadas.

--- main.py
+++ main.py
@@ -55,9 +55,10 @@ barriers = [barrier1, barrier2, barrier3, barrier4, barrier5, barrier6, barrier7
 N1s = neigh.Circle(center=[70, 55], radii=4, col = 'green')
 N2s = neigh.Circle(center=[50, 70], radii=8, col = 'green')
 N3s = neigh.Circle(center=[30, 35], radii=10, col = 'green')
+N4s = neigh.Circle(center=[5, 40], radii= 12, col = 'green')
 
-sources = [N1s, N2s, N3s]
-sources = [N1s]
+sources = [N1s, N2s, N3s, N4s]
+# sources = [N1s]
 
 N1t = neigh.Circle(center=[10, 15], radii=6, col = 'blue')
 N2t = neigh.Circle(center=[65, 10], radii=7, col = 'blue')
@@ -95,20 +96,20 @@ distancias = np.zeros((1000, 1000))
 # for b in barriers:
 #     ax.plot([b[0][0], b[1][0]], [b[0][1], b[1][1]], c='red')
 
+for s in range(len(sources)):
+    for i in range(1000):
+        for j in range(1000):
+            targets = [neigh.Circle(center=[x_coords[i], y_coords[j]], radii=0.01, col = 'blue')]
+            resultados = hspp_b(barriers, [sources[s]], targets, wL = 0, A4 = False, log=False, picture=False, time_limit=7200, init=False)
 
-for i in range(1000):
-    for j in range(1000):
-        targets = [neigh.Circle(center=[x_coords[i], y_coords[j]], radii=0.01, col = 'blue')]
-        resultados = hspp_b(barriers, sources, targets, wL = 0, A4 = False, log=False, picture=False, time_limit=7200, init=False)
+            distancias[i, j] = resultados[1]
 
-        distancias[i, j] = resultados[1]
+            # if resultados[1] < radii:
+            #     ax.scatter(xcor, ycor, color = 'blue')
+            # else:
+            #     ax.scatter(xcor, ycor, color = 'red')
 
-        # if resultados[1] < radii:
-        #     ax.scatter(xcor, ycor, color = 'blue')
-        # else:
-        #     ax.scatter(xcor, ycor, color = 'red')
-
-    np.savetxt('distancias_masbarreras.csv', distancias, delimiter=',')
+        np.savetxt('distancias_' + str(s) + '.csv', distancias, delimiter=',')
 # ax.scatter(sources[0].center[0], sources[0].center[1], c='green')
 
 # plt.axis([0, 100, 0, 100])
